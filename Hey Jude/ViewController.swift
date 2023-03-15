0581261fe42f848848bdff1e234c1add4a163725
Pei Chen <peggy_chen05@live.com>
2023-03-15 17:24:08 +0800

    Hey Jude _ Storyboard + Sugue + Background music

--- Hey Jude/ViewController.swift
+++ Hey Jude/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import AVFoundation
+
+class AudioManager {
+    static let shared = AudioManager()
+
+    private let player = AVQueuePlayer()
+
+    func play(url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player.insert(playerItem, after: nil)
+        player.play()
+    }
+
+    func pause() {
+        player.pause()
+    }
+
+    func stop() {
+        player.removeAllItems()
+    }
+}
 
 class ViewController: UIViewController {
+    let url = Bundle.main.url(forResource: "Hey-Jude", withExtension: "mp3")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        AudioManager.shared.play(url: url)
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        AudioManager.shared.pause()
+        // Other preparation code for the segue
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if let navigationController = self.navigationController,
+           !navigationController.viewControllers.contains(self) {
+            AudioManager.shared.stop()
+        }
+    }
 }
-
